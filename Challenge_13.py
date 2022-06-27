e304617ccd97bb519cafd11e8fba991b700d477f
Alexis Labrador <alexisnlh@outlook.com>
2022-06-27 10:47:29 +0200

    Resolución Reto #13

---
+++ Challenge_13.py
@@ -0,0 +1,44 @@
+# Reto #13
+# Factorial Recursivo
+# Fecha publicación enunciado: 28/03/22
+# Fecha publicación resolución: 04/04/22
+# Dificultad: FÁCIL
+
+# Enunciado: Escribe una función que calcule y retorne el factorial de un número dado de forma recursiva.
+
+
+# Función para calcular el factorial del número ingresado
+def factorial(input_string):
+    try:
+        if int(input_string) > 1:
+            number = int(input_string)
+            return number * factorial(number - 1)
+        elif 0 < int(input_string) <= 1:
+            return 1
+        else:
+            return None
+    except Exception as error:
+        print("Exception: {}".format(error))
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            input_string = input("Introduzca el número para calcular el factorial, y presione ENTER: \n")
+            input_string = input_string.rstrip().lstrip()
+            if input_string != "":
+                # Función para calcular el factorial del número ingresado
+                result = factorial(input_string)
+                if result is not None:
+                    print(f"El factorial del número ingresado, {input_string}, es: {result}")
+                else:
+                    print(f"El factorial del número ingresado, {input_string}, no se puede obtener. El número debe ser mayor a cero.")
+                break
+            else:
+                opcion = input("No se introdujo un texto válido. Desea continuar? (Y/N)\n").lower()
+                if opcion == "n" or "no" in opcion:
+                    break
+        except:
+            opcion = input("No se introdujo un texto válido. Desea continuar? (Y/N)\n").lower()
+            if opcion == "n" or "no" in opcion:
+                break
