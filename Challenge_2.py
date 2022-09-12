a6f355da12067d717d81bb7c99f507ccb9e6c0bb
Alexis Labrador <alexisnlh@outlook.com>
2022-09-12 10:12:17 +0200

    Modificación Reto #2 a Clases

--- Challenge_2.py
+++ Challenge_2.py
@@ -1,16 +1,22 @@
-# Reto #2
-# La sucesión de Fibonacci
-# Fecha publicación enunciado: 10/01/22
-# Fecha publicación resolución: 17/01/22
-# Dificultad: DIFÍCIL
+"""
+    Reto #2
+    La sucesión de Fibonacci
+    Fecha publicación enunciado: 10/01/22
+    Fecha publicación resolución: 17/01/22
+    Dificultad: DIFÍCIL
 
-# Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
-# La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
-# 0, 1, 1, 2, 3, 5, 8, 13...
+    Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+    La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+    0, 1, 1, 2, 3, 5, 8, 13...
+"""
 
-# Función para imprimir los 50 primeros números de la sucesión de Fibonacci
-def fibonacci():
-    try:
+
+class Fibonacci:
+    """
+        Función para imprimir los 50 primeros números de la sucesión de Fibonacci
+    """
+    @staticmethod
+    def fibonacci_numbers():
         num_0 = 0
         num_1 = 1
         for number in range(1, 51):
@@ -18,10 +24,7 @@ def fibonacci():
             num_next = num_0 + num_1
             num_0 = num_1
             num_1 = num_next
-    except Exception as error:
-        print("Exception: {}".format(error))
 
 
 if __name__ == '__main__':
-    # Función para imprimir los 50 primeros números de la sucesión de Fibonacci
-    fibonacci()
+    Fibonacci.fibonacci_numbers()
