4a74548d666dabeb4cae1584a7bfa78c9159009f
Alexis Labrador <alexisnlh@outlook.com>
2022-09-14 10:33:15 +0200

    Modificación Reto #3 a Clases

--- Challenge_3.py
+++ Challenge_3.py
@@ -1,31 +1,36 @@
-# Reto #3
-# ¿Es un número primo?
-# Fecha publicación enunciado: 17/01/22
-# Fecha publicación resolución: 24/01/22
-# Dificultad: MEDIA
+"""
+    Reto #3
+    ¿Es un número primo?
+    Fecha publicación enunciado: 17/01/22
+    Fecha publicación resolución: 24/01/22
+    Dificultad: MEDIA
 
-# Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
-# Hecho esto, imprime los números primos entre 1 y 100.
+    Enunciado: Escribe un programa que se encargue de comprobar si un número es o no primo.
+    Hecho esto, imprime los números primos entre 1 y 100.
+"""
 
 
-# Función para comprobar si un número es o no primo
-def esPrimo(number, num=2):
-    try:
-        if number < 1:
-            return False
-        if number == num:
-            return True
-        elif number % num != 0:
-            return esPrimo(number, num=num + 1)
+class EsPrimo:
+    """
+        Comprobar si un número es o no primo
+    """
+    def __init__(self, number, check_number):
+        self.number = number
+        self.check_number = check_number
+
+    def print_number(self):
+        if self.number <= 1:
+            pass
         else:
-            return False
-    except Exception as error:
-        print("Exception: {}".format(error))
+            if self.number == self.check_number:
+                print(self.number)
+            elif self.number % self.check_number != 0:
+                EsPrimo(self.number, self.check_number + 1).print_number()
+            else:
+                pass
 
 
 if __name__ == '__main__':
-    for number in range(1, 101):
-        # Función para comprobar si un número es o no primo
-        result = esPrimo(number)
-        if result:
-            print(number)
+    for index in range(1, 101):
+        # Comprueba si un número es o no primo
+        EsPrimo(index, 2).print_number()
