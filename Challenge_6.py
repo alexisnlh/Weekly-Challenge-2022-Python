f2169870adb5cd48a0105692c302c3537eecf410
Alexis Labrador <alexisnlh@outlook.com>
2022-11-11 20:11:16 +0100

    Modificado Reto #6 a Clases

--- Challenge_6.py
+++ Challenge_6.py
@@ -6,7 +6,7 @@
     Dificultad: FÁCIL
 
     Enunciado: Crea un programa que invierta el orden de una cadena de texto
-    sin usar funciones propias del lenguaje que lo hagan de forma automática
+    sin usar funciones propias del lenguaje que lo hagan de forma automática.
         - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
 """
 
