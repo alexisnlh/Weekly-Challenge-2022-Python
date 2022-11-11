596030db93ee7cd652fc3ef6e67600f9ca2caae0
Alexis Labrador <alexisnlh@outlook.com>
2022-11-11 19:55:11 +0100

    Modificado Reto #6 a Clases

--- Challenge_6.py
+++ Challenge_6.py
@@ -69,48 +69,6 @@ class recursiveReverse:
         return newReversedText
 
 
-# # Función 1 para introducir la cadena de texto y devolver su inversa. Con un bucle
-# def reverse(input_string):
-#     try:
-#         # Obtiene el tamaño de la cadena de texto - 1
-#         textCount = len(input_string) - 1
-#         # Almacena en una lista cada letra de la cadena de texto
-#         textArray = list(input_string)
-#         reversedText = str()
-#
-#         for index in range(0, textCount + 1):
-#             # Contruye la nueva cadena de texto invertida obteniendo la letra por su posición en la lista inicial (textArray)
-#             reversedText += textArray[textCount - index]
-#
-#         return reversedText
-#     except Exception as error:
-#         print("Exception: {}".format(error))
-
-
-# Funcion 2 para introducir la cadena de texto y devolver su inversa. Sin un bucle, mediante una función recursiva
-# def recursiveReverse(input_string, index=0, reversedText=""):
-#     try:
-#         # Obtiene el tamaño de la cadena de texto - 1
-#         textCount = len(input_string) - 1
-#         # Almacena en una lista cada letra de la cadena de texto
-#         textArray = list(input_string)
-#         # Almacena la nueva cadena de texto invertida por cada recursividad
-#         newReversedText = reversedText
-#         # Se agrega a la nueva cadena de texto invertida la letra ubicada en la posición que se pasa por la variable index en cada recursividad de la lista inicial (textArray)
-#         newReversedText += textArray[textCount - index]
-#
-#         # Se verifica que la cuenta (index) sea menor que el tamaño de la cadena de texto inicial - 1
-#         if index < textCount:
-#             # Se aumenta la cuenta index
-#             newIndex = index + 1
-#             # Se llama a la función para ir construyendo la cadena de texto inversa
-#             newReversedText = recursiveReverse(input_string, index=newIndex, reversedText=newReversedText)
-#
-#         return newReversedText
-#     except Exception as error:
-#         print("Exception: {}".format(error))
-
-
 if __name__ == '__main__':
     flag_continue = True
     while flag_continue:
