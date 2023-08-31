1074911ea10f6024c5a9d9acd9d4e89b42246f2d
Alexis Labrador <alexisnlh@outlook.com>
2023-08-31 17:50:28 +0200

    Modificación Reto #11 a Clases y formato de letras y colores

--- Challenge_11.py
+++ Challenge_11.py
@@ -1,3 +1,9 @@
+import sys
+import typer
+from rich.console import Console
+from rich.prompt import Prompt
+
+console = Console(color_system="windows")
 # Reto #11
 # Eliminando caracteres
 # Fecha publicación enunciado: 14/03/22
@@ -9,81 +15,78 @@
 # - out2 contendrá todos los caracteres presentes en la str2 pero NO estén presentes en str1.
 
 
-# Función para verificar las dos cadenas de caracteres ingresadas
-def printNonCommon(str1, str2):
-    list_1 = list()
-    list_1[:0] = str1
-    list_2 = list()
-    list_2[:0] = str2
-    out1 = list()
-    out2 = list()
+class DeleteCharacters:
+    """
+        Verificar las dos cadenas de caracteres ingresadas
+    """
+    def __init__(self, input_string_1, input_string_2):
+        self.input_string_1 = input_string_1
+        self.input_string_2 = input_string_2
 
-    for letter in list_1:
-        if letter not in str2:
-            out1.append(letter)
-    out1 = "".join(out1)
+    def print_non_common(self):
+        list_1 = list()
+        list_1[:0] = self.input_string_1
+        list_2 = list()
+        list_2[:0] = self.input_string_2
+        out1 = list()
+        out2 = list()
 
-    for letter in list_2:
-        if letter not in str1:
-            out2.append(letter)
-    out2 = "".join(out2)
+        for letter in list_1:
+            if letter not in self.input_string_2:
+                out1.append(letter)
+        out1 = "".join(out1)
 
-    return out1, out2
+        for letter in list_2:
+            if letter not in self.input_string_1:
+                out2.append(letter)
+        out2 = "".join(out2)
+
+        console.print(
+            f"Los caracteres presentes en la primera cadena pero no en la segunda cadena son (utilizando función [bold]print_non_common[/bold]): [green]{out1}[/green]\nLos caracteres presentes en la segunda cadena pero no en la primera cadena son (utilizando función [bold]print_non_common[/bold]): [blue]{out2}[/blue]")
+
+    def print_non_common_with_filter(self):
+        list_1 = list()
+        list_1[:0] = self.input_string_1
+        list_2 = list()
+        list_2[:0] = self.input_string_2
+
+        out1 = [letter for letter in list_1 if letter not in self.input_string_2]
+        out1 = "".join(out1)
+        out2 = [letter for letter in list_2 if letter not in self.input_string_1]
+        out2 = "".join(out2)
+
+        console.print(
+            f"\nLos caracteres presentes en la primera cadena pero no en la segunda cadena son (utilizando función [bold]print_non_common_with_filter[/bold]): [green]{out1}[/green]\nLos caracteres presentes en la segunda cadena pero no en la primera cadena son (utilizando función [bold]print_non_common_with_filter[/bold]): [blue]{out2}[/blue]")
 
 
-# Otra solución utilizando funciones de orden superior
-def printNonCommonWithFilter(str1, str2):
-    list_1 = list()
-    list_1[:0] = str1
-    list_2 = list()
-    list_2[:0] = str2
+def main(input_string_1=None):
+    if input_string_1 is None:
+        input_string_1 = Prompt.ask("Introduzca la primera cadena de caracteres y presione ENTER (q --> Exit)").lower()
+        if input_string_1 == "":
+            console.print(
+                f"[red]No se introdujo la primera cadena de caracteres válida![/red]")
+            main()
+            sys.exit("Proceso finalizado!")
 
-    out1 = [letter for letter in list_1 if letter not in str2]
-    out1 = "".join(out1)
-    out2 = [letter for letter in list_2 if letter not in str1]
-    out2 = "".join(out2)
+        elif input_string_1 == "q":
+            sys.exit("Proceso finalizado!")
 
-    return out1, out2
+    input_string_2 = Prompt.ask("Introduzca la segunda cadena de caracteres y presione ENTER (q --> Exit)").lower()
+    if input_string_2 == "":
+        console.print(
+            f"[red]No se introdujo la segunda cadena de caracteres válida![/red]")
+        main(input_string_1=input_string_1)
+        sys.exit("Proceso finalizado!")
+
+    elif input_string_2 == "q":
+        sys.exit("Proceso finalizado!")
+
+    # Verificar las dos cadenas de caracteres ingresadas
+    DeleteCharacters(input_string_1, input_string_2).print_non_common()
+
+    # Otra solución utilizando funciones de orden superior
+    DeleteCharacters(input_string_1, input_string_2).print_non_common_with_filter()
 
 
 if __name__ == '__main__':
-    flag_continue = True
-    while flag_continue:
-        try:
-            str1 = input("Introduzca la primera cadena de caracteres y presione ENTER: \n")
-            str2 = input("Introduzca la segunda cadena de caracteres y presione ENTER: \n")
-            if str1 != "" and str2 != "":
-                # Función para verificar las dos cadenas de caracteres ingresadas
-                out1, out2 = printNonCommon(str1.lower(), str2.lower())
-                print(f"Los caracteres presentes en la primera cadena pero no en la segunda cadena son (utilizando función printNonCommon): {out1}")
-                print(f"Los caracteres presentes en la segunda cadena pero no en la primera cadena son (utilizando función printNonCommon): {out2}")
-
-                # Otra solución utilizando funciones de orden superior
-                out1, out2 = printNonCommonWithFilter(str1.lower(), str2.lower())
-                print(f"\nLos caracteres presentes en la primera cadena pero no en la segunda cadena son (utilizando función printNonCommonWithFilter): {out1}")
-                print(f"Los caracteres presentes en la segunda cadena pero no en la primera cadena son (utilizando función printNonCommonWithFilter): {out2}")
-                break
-            elif str1 == "":
-                while True:
-                    opcion = input("No se introdujo la primera cadena de caracteres válida. Desea continuar? (Y/N)\n").lower()
-                    if opcion == "y" or "yes" in opcion:
-                        break
-                    elif opcion == "n" or "no" in opcion:
-                        flag_continue = False
-                        break
-            elif str2 == "":
-                while True:
-                    opcion = input("No se introdujo la segunda cadena de caracteres válida. Desea continuar? (Y/N)\n").lower()
-                    if opcion == "y" or "yes" in opcion:
-                        break
-                    elif opcion == "n" or "no" in opcion:
-                        flag_continue = False
-                        break
-        except:
-            while True:
-                opcion = input("No se introdujo la cadena de caracteres válida. Desea continuar? (Y/N)\n").lower()
-                if opcion == "y" or "yes" in opcion:
-                    break
-                elif opcion == "n" or "no" in opcion:
-                    flag_continue = False
-                    break
+    typer.run(main)
