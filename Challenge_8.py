b441b0ebbffc889745474973ed2db189c9d7b2ed
Alexis Labrador <alexisnlh@outlook.com>
2023-08-31 13:55:26 +0200

    Modificación Reto #8 a Clases y formato de letras y colores

--- Challenge_8.py
+++ Challenge_8.py
@@ -1,6 +1,11 @@
-import math
+import sys
 import re
+import math
+import typer
+from rich.console import Console
+from rich.prompt import Prompt, FloatPrompt
 
+console = Console(color_system="windows")
 # Reto #8
 # Decimal a binario
 # Fecha publicación enunciado: 18/02/22
@@ -10,47 +15,31 @@ import re
 # Enunciado: Crea un programa que se encargue de transformar un número decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
 
 
-# Función para contar las veces que se repite cada palabra
-def decimalToBinary(input_string):
-    try:
-        number = round(float(input_string))
+class DecimalToBinary:
+    """
+        Transformar número decimal a binario
+    """
+    def __init__(self, input_decimal):
+        self.input_decimal = input_decimal
+
+    def transform_process(self):
+        number = round(float(self.input_decimal))
         binary = str()
 
         while number > 0:
             reminder = number % 2
             number //= 2
             binary = str(reminder) + binary
-        return binary if round(float(input_string)) > 0 else 0
-    except Exception as error:
-        print("Exception: {}".format(error))
+        console.print(
+            f"El binario del número decimal {self.input_decimal} es: [bold]{binary if round(float(self.input_decimal)) > 0 else 0}[/bold]")
+
+
+def main():
+    input_decimal = FloatPrompt.ask(
+        "Introduzca un número decimal, distinto de cero y presione ENTER")
+
+    DecimalToBinary(input_decimal).transform_process()
 
 
 if __name__ == '__main__':
-    flag_continue = True
-    while flag_continue:
-        try:
-            input_string = input("Introduzca un número decimal, distinto de cero y presione ENTER: \n")
-            input_string = input_string.rstrip().lstrip()
-
-            # Comprueba si es formato decimal el texto ingresado
-            it_is = re.match(re.compile(r'^\-?[1-9][0-9]*\.?[0-9]*$'), input_string)
-            if input_string != "" and it_is:
-                # Función para contar las veces que se repite cada palabra
-                print(f"El binario del número decimal {input_string} es: {decimalToBinary(input_string)}")
-                break
-            else:
-                while True:
-                    opcion = input("No se introdujo una cadena de texto válida. Desea continuar? (Y/N)\n").lower()
-                    if opcion == "y" or "yes" in opcion:
-                        break
-                    elif opcion == "n" or "no" in opcion:
-                        flag_continue = False
-                        break
-        except:
-            while True:
-                opcion = input("No se introdujo una cadena de texto válida. Desea continuar? (Y/N)\n").lower()
-                if opcion == "y" or "yes" in opcion:
-                    break
-                elif opcion == "n" or "no" in opcion:
-                    flag_continue = False
-                    break
+    typer.run(main)
